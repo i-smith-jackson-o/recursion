b156aca0cd260819a619f2c0f4f53839b1d4b3b1
i-smith-jackson-o <i.smith.jackson.o@gmail.com>
2023-01-02 22:04:06 -0500

    still headache

--- problems/10-factorial.js
+++ problems/10-factorial.js
@@ -17,6 +17,13 @@ factorial(5); // 120
 ***********************************************************************/
 
 // your code here
+function factorial(n) {
+  if (n === 0) {
+    return 1; 
+  } else {
+    return n * factorial(n - 1); 
+  }
+}
   
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -11,6 +11,19 @@ flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
 // your code here
+function flatten(arr) {
+  let flattened = []; 
+  for(let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      flattened = flattened.concat(flatten(arr[i])); 
+    } else {
+      flattened.push(arr[i]); 
+    }
+  }
+  return flattened; 
+}
+
+
   
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
